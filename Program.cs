5d11509cf8164870c34bfd83abc5e64cc3b555b9
Jakub Erbetowski <s27747@pjwstk.edu.pl>
2026-03-27 11:33:44 +0100

    Show min value in program

--- Program.cs
+++ Program.cs
@@ -7,10 +7,14 @@ try
     int sum = StatisticsHelper.CalculateSum(values);
     double average = StatisticsHelper.CalculateAverage(values);
     int max = StatisticsHelper.CalculateMax(values);
+    int min = StatisticsHelper.CalculateMin(values);
+
     
     Console.WriteLine($"Sum: {sum}");
     Console.WriteLine($"Average: {average:F2}");
     Console.WriteLine($"Max: {max}");
+    Console.WriteLine($"Min: {min}");
+
 
 }
 catch (Exception exception)
