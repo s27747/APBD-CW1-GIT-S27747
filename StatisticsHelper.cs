7460dd7568a4cde93fe47577481c28e70ea99bc7
Jakub Erbetowski <s27747@pjwstk.edu.pl>
2026-03-27 10:54:30 +0100

    Add input validation

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -4,6 +4,16 @@ public class StatisticsHelper
 {
     public static int CalculateSum(int[] values)
     {
+        if (values == null)
+        {
+            throw new ArgumentNullException(nameof(values));
+        }
+
+        if (values.Length == 0)
+        {
+            throw new ArgumentException("Array cannot be empty.");
+        }
+
         int sum = 0;
 
         foreach (int value in values)
