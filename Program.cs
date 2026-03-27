fda6af511a4e2ee44a4fd1e21f44b4431d37adfc
Jakub Erbetowski <s27747@pjwstk.edu.pl>
2026-03-27 10:55:12 +0100

    Show error message for invalid input

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 ﻿using APBD_CW1_GIT_S27747;
 
-int[] values = { 1, 2, 3, 4, 5 };
+int[] values = { };
 
-int sum = StatisticsHelper.CalculateSum(values);
-
-Console.WriteLine($"Sum: {sum}");
+try
+{
+    int sum = StatisticsHelper.CalculateSum(values);
+    Console.WriteLine($"Sum: {sum}");
+}
+catch (Exception exception)
+{
+    Console.WriteLine($"Error: {exception.Message}");
+}
