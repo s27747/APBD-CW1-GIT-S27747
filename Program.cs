37ce4f25a770e5180e88d8e9c05cd19ea44ec1a3
Jakub Erbetowski <s27747@pjwstk.edu.pl>
2026-03-27 10:53:00 +0100

    Add sum calculation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using APBD_CW1_GIT_S27747;
+
+int[] values = { 1, 2, 3, 4, 5 };
+
+int sum = StatisticsHelper.CalculateSum(values);
+
+Console.WriteLine($"Sum: {sum}");
--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -2,5 +2,15 @@ namespace APBD_CW1_GIT_S27747;
 
 public class StatisticsHelper
 {
-    
+    public static int CalculateSum(int[] values)
+    {
+        int sum = 0;
+
+        foreach (int value in values)
+        {
+            sum += value;
+        }
+
+        return sum;
+    }
 }
